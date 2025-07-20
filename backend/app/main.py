e86a8af16644c08de5981422b8dac4c601569b5d
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-20 17:15:14 -0500

    feat(backend): :sparkles: add MovieRepository manage crud in memory

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,18 @@
-from fastapi import FastAPI
 from app.routers.movies import router as movie_router
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/', tags=['Home'])
+@app.get("/", tags=["Home"])
 def home():
     return "Hola universo!"
 
 
-@app.get('/hello/{name}', tags=['Home'])
+@app.get("/hello/{name}", tags=["Home"])
 def greeting(name):
-    response = {
-        'message': f'Hello {name}'
-    }
+    response = {"message": f"Hello {name}"}
     return response
 
 
