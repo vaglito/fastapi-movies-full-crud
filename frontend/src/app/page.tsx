9eeed354e86a2589b8a998fe2a5204d5b6c0b7a9
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-25 13:11:37 -0500

    style(frontend): :fire: code formatting and translation

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,17 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Billboard } from "@/components/billboard";
+import { Container } from "@chakra-ui/react";
+import { Button, Stack, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">Welcome to Movie API</h1>
-      <p className="mt-4">Explore the world of movies with our API.</p>
-      <HStack>
-        <Button colorPalette="green">Click me</Button>
-      </HStack>
-    </main>
+    <Container>
+      <Heading as="h1" size="3xl">
+        Bienvenido a Cine Movie
+      </Heading>
+      <Text>Explore la amplia gama de peliculas en nuestra cartelera.</Text>
+      <Stack>
+        <Billboard />
+      </Stack>
+    </Container>
   );
 }
