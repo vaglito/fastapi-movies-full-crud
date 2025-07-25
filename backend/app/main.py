03c95e6cc2bed206c8d34b9421f24a40330915ce
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-24 20:39:06 -0500

    feat(backend): :sparkles: add cors headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,17 @@
 from app.routers.movies import router as movie_router
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],            # Permitir estos orígenes
+    allow_credentials=True,           # Permitir cookies/autenticación
+    allow_methods=["*"],              # Permitir todos los métodos (GET, POST, etc)
+    allow_headers=["*"],              # Permitir todos los headers
+)
 
 @app.get("/", tags=["Home"])
 def home():
