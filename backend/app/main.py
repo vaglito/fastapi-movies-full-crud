ff5c0793a597790d77799df454895cbac408fd80
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-07-26 01:16:36 -0500

    refactor(backend): clean up CORS middleware configuration and remove redundant code

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,17 +2,17 @@ from app.routers.movies import router as movie_router
 from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],            # Permitir estos orígenes
-    allow_credentials=True,           # Permitir cookies/autenticación
-    allow_methods=["*"],              # Permitir todos los métodos (GET, POST, etc)
-    allow_headers=["*"],              # Permitir todos los headers
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 @app.get("/", tags=["Home"])
 def home():
@@ -24,13 +24,5 @@ def greeting(name):
     response = {"message": f"Hello {name}"}
     return response
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(movie_router)
